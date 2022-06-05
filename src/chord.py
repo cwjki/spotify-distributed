bc5649c1cb3371a4d0f122aa8a4831002a553ef8
cwjki <cwjki97@gmail.com>
2022-06-05 12:39:51 -0400

    updating local nodes finger tables

--- src/chord.py
+++ src/chord.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from typing_extensions import Self
 import Pyro4
 from utils import hashing, get_node_instance, print_node_info
 
@@ -136,3 +137,63 @@ class ChordNode:
 
         print_node_info(self)
         return True
+
+    def init_finger_table(self, node):
+        '''
+        Initialize the local finger table of a node
+        '''
+        self.successor = node.find_successor(self._finger_table_start[1]).id
+        self.predecessor = self.successor.node_finger_table[0]
+
+        successor_keys = self.successor.keys.keys()
+        for key in successor_keys:
+            if self.in_range(key, self.node_finger_table[0] + 1, self.id + 1):
+                self.keys[key] = self.successor.pop_key(key)
+
+        self.successor.successor.predecessor_keys = self.successor.keys
+        self._predecessor_keys = self.predecessor.keys
+        self.successor.predecessor = self.id
+
+        for i in range(1, self.size):
+            if self.in_range(self._finger_table_start[i+1], self.id, self._node_finger_table[i]):
+                self._node_finger_table[i+1] = self.node_finger_table[i]
+            else:
+                self._node_finger_table[i+1] = node.find_successor(
+                    self._node_finger_table[i+1]).id
+
+    def pop_key(self, key):
+        '''
+        Delete a key in the local node and returns its value
+        '''
+        try:
+            value = self._keys.pop(key)
+            print(f'Key {key} was deleted in node {self.id}')
+            return value
+        except KeyError:
+            print(f'KeyError: Could not delete key {key} in node {self.id}')
+            return None
+
+    def update_others(self):
+        '''
+        Update all nodes whose finger table should refer to a local node 
+        '''
+        for i in range(1, self.size + 1):
+            predecessor = self.find_predecessor((self.id - pow(2, i-1)) % self.size)
+            if predecessor and predecessor.id != self.id:
+                predecessor.update_finger_table(self.id, i)
+    
+    def update_finger_table(self, s, i):
+        '''
+        If s is the ith finger of the local node, update local node 
+        finger table with s
+        '''
+        if self.in_range(s, self.id, self._finger_table[i]):
+            if i == 1:
+                self.succesors = s
+            else:
+                self._node_finger_table[i] = s
+            predecessor = self.predecessor
+            if predecessor and predecessor.id != s:
+                predecessor.update_finger_table(s, i)
+
+    
