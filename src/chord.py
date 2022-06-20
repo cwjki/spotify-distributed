0670afcd9d43b2c6e3e49262ae43d32d0c1a006b
cwjki <cwjki97@gmail.com>
2022-06-20 13:20:31 -0400

    changing the way we save the data in the chord nodes

--- src/chord.py
+++ src/chord.py
@@ -198,23 +198,23 @@ class ChordNode:
             else:
                 self._node_finger_table[i] = s
             predecessor = self.predecessor
-            if predecessor and predecessor.id != s:
+            if predecessor and predecessor._id != s:
                 predecessor.update_finger_table(s, i)
 
     def stabilize(self):
         '''
-        Periodically verify local node inmediate successor and 
+        Periodically verify local node's inmediate successor and 
         tell the successor about local node
         '''
-        while self.successor is None:
-            if not self._successor_list:
-                self.successor = self.id
-                return
-            self.successor = self._successor_list.pop(0)
+        if self.successor is None:
+            if self._successor_list:
+                self.successor = self._successor_list.pop(0)
+            else:
+                self.successor = self._id
 
         node = self.successor.predecessor
-        if node and self.in_range(node.id, self.id+1, self._node_finger_table[1]) and ((self.id + 1) % self.size) != self._node_finger_table[1]:
-            self.successor = node.id
+        if node and self.in_range(node._id, self._id + 1, self._node_finger_table[1]) and ((self._id + 1) % self.size) != self._node_finger_table[1]:
+            self.successor = node._id
         if self.successor:
             self.successor.notify(self)
 
@@ -225,11 +225,11 @@ class ChordNode:
         if not self.predecessor:
             for key in self._predecessor_keys.keys():
                 self._keys[key] = self._predecessor_keys[key]
-                if self.successor and self.successor.id != self.id:
+                if self.successor and self.successor._id != self._id:
                     self.successor.update_predecessor_key(key, self._keys[key])
 
-        if not self.predecessor or self.in_range(node.id, self._node_finger_table[0] + 1, self.id):
-            self.predecessor = node.id
+        if not self.predecessor or self.in_range(node._id, self._node_finger_table[0] + 1, self._id):
+            self.predecessor = node._id
 
     def update_predecessor_key(self, key, value):
         '''
@@ -240,41 +240,83 @@ class ChordNode:
         except:
             self._predecessor_keys[key] = value
 
-    def refresh_fingers(self):
+    def fix_fingers(self):
         '''
         Periodically refresh finger table
         '''
-        i = random.randint(2, self.size)
+        i = random.randint(2, self.m)
         node = self.find_successor(self._finger_table_start[i])
         if node:
-            self._node_finger_table[i] = node.id
+            self._node_finger_table[i] = node._id
 
     def update_successor_list(self):
         while True:
             try:
                 if not self._successor_list:
-                    if self.successor and self.successor.id != self.id:
-                        self._successor_list.append(self.successor.id)
+                    if self.successor and self.successor._id != self._id:
+                        self._successor_list.append(self.successor._id)
 
-                elif len(self._successor_list) < self.size:
+                elif len(self._successor_list) < self.m:
                     for i in range(len(self._successor_list)):
                         succ = get_node_instance(self._successor_list[i])
                         if succ:
                             new_succ = succ.successor
-                            if new_succ and new_succ.id != self.id and new_succ not in self._successor_list:
-                                self._successor_list.insert(i+1, new_succ.id)
+                            if new_succ and new_succ._id != self._id and new_succ._id not in self._successor_list:
+                                self._successor_list.insert(i+1, new_succ._id)
                                 break
             except:
                 pass
 
             time.sleep(1)
 
+    def lookup(self, key):
+        '''
+        Return the node responsible for storing the key
+        '''
+        while True:
+            if self.in_range(key, self.node_finger_table[0] + 1, self._id + 1):
+                return self
+            else:
+                for k in range(1, self.m):
+                    if self.in_range(key, self._finger_table_start[k], self._finger_table_start[k + 1]):
+                        node = get_node_instance(self.node_finger_table[k])
+                        if node:
+                            return node.lookup(key)
+                else:
+                    node = get_node_instance(self.node_finger_table[self.m])
+                    if node:
+                        return node.lookup(key)
+
+    def save_key(self, key, value):
+        '''
+        Save a key and its value in the Chord Ring, 
+        if operation was successfully return True
+        else return False
+        '''
+        node = self.lookup(key)
+        if node:
+            successor = node.update_key(key, value)
+
+    def store_key(self, key, value):
+        '''
+        Update key and value
+        '''
+        try:
+            for k, v in self.keys[key]:
+                if k == value[0]:
+                    return False
+            self._keys[key].append(value)
+        except:
+            self.keys[key] = [value]
+
+        return True
+
 
 def stabilize_function(node: ChordNode):
     while True:
         try:
             node.stabilize()
-            node.refresh_fingers()
+            node.fix_fingers()
             time.sleep(1)
         except:
             pass
