edeabaf69b2504bdd6070b3641a9ac28a3c77928
cwjki <cwjki97@gmail.com>
2022-06-21 16:24:57 -0400

    frontend structure done, need to make the connections with the spotify nodes to handle request

--- src/client.py
+++ src/client.py
@@ -1,4 +1,3 @@
-from sys import flags
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 
@@ -9,6 +8,38 @@ Bootstrap(app)
 @app.route("/", methods=['GET'])
 def home():
     return render_template('home.html')
+
+
+@app.route("/all-songs", methods=['GET'])
+def all_songs():
+    flag = 'songs'
+    metadata = 'todas las canciones'
+    data = []
+    return render_template('result.html', content=[flag, metadata, data])
+
+
+@app.route("/upload-song", methods=['GET', 'POST'])
+def upload_song():
+    if request.method == 'POST':
+        title = request.form['title']
+        gender = request.form['gender']
+        author = request.form['author']
+        song = request.form['song']
+        return render_template('home.html')
+    else:
+        return render_template('upload_song.html')
+
+
+@app.route("/search-by-title", methods=['GET', 'POST'])
+def search_by_title():
+    if request.method == 'POST':
+        title = request.form['title']
+        flag = 'title'
+        metadata = title
+        data = []
+        return render_template('result.html', content=[flag, metadata, data])
+    else:
+        return render_template('search_by_title.html')
 
 
 @app.route("/search-by-gender", methods=['GET', 'POST'])
