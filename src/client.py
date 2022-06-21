c79c975c3bed4d904f0787cfcedd21b844a31e33
cwjki <cwjki97@gmail.com>
2022-06-21 14:19:35 -0400

    working in the front, search by author and gender pages done

--- src/client.py
+++ src/client.py
@@ -1,3 +1,4 @@
+from sys import flags
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 
@@ -5,10 +6,34 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=['GET'])
 def home():
     return render_template('home.html')
 
 
-if __name__ == "__client__":
+@app.route("/search-by-gender", methods=['GET', 'POST'])
+def search_by_gender():
+    if request.method == 'POST':
+        gender = request.form['gender']
+        flag = 'gender'
+        metadata = gender
+        data = []
+        return render_template('result.html', content=[flag, metadata, data])
+    else:
+        return render_template('search_by_gender.html')
+
+
+@app.route("/search-by-author", methods=['GET', 'POST'])
+def search_by_author():
+    if request.method == 'POST':
+        author = request.form['author']
+        flag = 'author'
+        metadata = author
+        data = []
+        return render_template('result.html', content=[flag, metadata, data])
+    else:
+        return render_template('search_by_author.html')
+
+
+if __name__ == "__main__":
     app.run(debug=True)
