4cea850ee7015ed291a516d70212ab170731ce80
cwjki <cwjki97@gmail.com>
2022-06-21 12:54:35 -0400

    preparing spotify nodes

--- src/spotify.py
+++ src/spotify.py
@@ -1,3 +1,6 @@
+import sys
+import threading
+from urllib import request
 import Pyro4
 from utils import hashing, get_spotify_node_instance
 
@@ -26,7 +29,7 @@ class SpotifyNode:
             node = get_spotify_node_instance(spotify_address)
             if node:
                 try:
-                    spotify_nodes = self.spotify_nodes_list
+                    spotify_nodes = node.spotify_nodes_list
                     for addr in spotify_nodes:
                         if addr == self.address:
                             continue
@@ -39,3 +42,34 @@ class SpotifyNode:
             else:
                 return False
         return True
+
+
+def main(address, spotify_address, chord_address, bits):
+    host_ip, host_port = address.split(':')
+    try:
+        deamon = Pyro4.Daemon(host=host_ip, port=int(host_port))
+    except:
+        print('Error: There is another node in the system with that address, please try another')
+        return
+
+    spotify_node = SpotifyNode(address, chord_address, bits)
+    uri = deamon.register(spotify_node)
+    ns = Pyro4.locateNS()
+    ns.register(f'SPOTIFY{address}', uri)
+
+    if spotify_node.join(spotify_address):
+        request_thread = threading.Thread(target=deamon.requestLoop)
+        request_thread.start()
+
+    else:
+        print(
+            f'Error: Could not connect to the network, no spotify nodes with address: {spotify_address}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        main(sys.argv[1], None, sys.argv[2], int(sys.argv[3]))
+    elif len(sys.argv) == 5:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
+    else:
+        print('Error: Missing arguments')
