95a96365571d127aa4139624b505b08eb7fcbf49
cwjki <cwjki97@gmail.com>
2022-06-21 13:35:05 -0400

    working on the front

--- src/client.py
+++ src/client.py
@@ -1,4 +1,14 @@
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 
+if __name__ == "__client__":
+    app.run(debug=True)
